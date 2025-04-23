f6ac8a3251f9026116fcf325299a53dd199ea9f7
whesleydiasmaciel <whesleydiasmaciel@gmail.com>
2025-04-23 17:59:57 +0000

    Exemplo

--- exemplo01.js
+++ exemplo01.js
@@ -6,16 +6,18 @@ function lerEntrada() {
     var idade = document.getElementById("idade").value;
 
     //Apresentar a informação no painel de resposta:
-    if (idade != "" && idade != undefined && idade != null ) {
+    if (idade != "" && idade != undefined && idade != null) {
         document.getElementById("resp").innerHTML = `<div class='tile'>Idade informada: ${idade} </div>`;
         votacao(idade);
     }
 }
 
 function votacao(idade) {
-    if (idade < 18)
+    if (idade < 18) {
         document.getElementById("resp").innerHTML += "<div class='tile'>A pessoa é menor de idade.</div>";
-    else
-        document.getElementById("resp").innerHTML += "<div class='tile'>A pessoa é maior de idade.</div>";
-
+        document.getElementById("resp").innerHTML += "<div class='tile'>A pessoa não pode votar.</div>";
+    }
+    else { }
+    document.getElementById("resp").innerHTML += "<div class='tile'>A pessoa é maior de idade.</div>";
+    document.getElementById("resp").innerHTML += "<div class='tile'>A pessoa pode votar.</div>";
 }
